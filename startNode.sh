e950448dbce523c13c79d733c9e4b493c27dafb4
harry3doz <k.hiroyuki.perc@gmail.com>
2022-10-25 14:53:10 +0900

    fix bug: duplicate name of nodes

--- startNode.sh
+++ startNode.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-eval echo "[unitree_camera_utils]" >> .startlog
+#eval echo "[unitree_camera_utils]" >> .startlog
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 ipLastSegment=15
 export SUDO_ASKPASS=passwd.sh
 
 
+sleep 10
 
 localIP=`cat /etc/network/interfaces | grep "address" | awk '{print $2}'`
 export ROS_MASTER_URI=http://192.168.123.161:11311
@@ -25,15 +26,19 @@ elif [ "${patchlines}" == "2" ]; then
     camNum1=$(cat patch | head -n 1 | awk '{print $2}')
     camNum2=$(cat patch | tail -n 1 | awk '{print $2}')
     if [ "${camNum1}" == "2" ]; then
-        rosrun unitree_camera_utils example_point ${SCRIPT_DIR}/config/stereo_camera_config.yaml &
+        #rosrun unitree_camera_utils example_point ${SCRIPT_DIR}/config/stereo_camera_config.yaml &
+        rosrun unitree_camera_utils pub_image _run_config_file:=${SCRIPT_DIR}/config/stereo_camera_config.yaml camera${camNum1} &
     else
-        rosrun unitree_camera_utils point_cloud_node _run_config_file:=${SCRIPT_DIR}/config/stereo_camera_config.yaml camera${camNum1} ${timeoffset} &
+        #rosrun unitree_camera_utils point_cloud_node _run_config_file:=${SCRIPT_DIR}/config/stereo_camera_config.yaml camera${camNum1} ${timeoffset} &
+        rosrun unitree_camera_utils pub_image _run_config_file:=${SCRIPT_DIR}/config/stereo_camera_config.yaml camera${camNum1} &
     fi
     sleep 5
     if [ "${camNum2}" == "2" ]; then
-        rosrun unitree_camera_utils example_point ${SCRIPT_DIR}/config/stereo_camera_config1.yaml &
+        #rosrun unitree_camera_utils example_point ${SCRIPT_DIR}/config/stereo_camera_config1.yaml &
+        rosrun unitree_camera_utils pub_image _run_config_file:=${SCRIPT_DIR}/config/stereo_camera_config.yaml camera${camNum2} &
     else
-        rosrun unitree_camera_utils point_cloud_node _run_config_file:=${SCRIPT_DIR}/config/stereo_camera_config1.yaml camera${camNum2} ${timeoffset} &
+        #rosrun unitree_camera_utils point_cloud_node _run_config_file:=${SCRIPT_DIR}/config/stereo_camera_config1.yaml camera${camNum2} ${timeoffset} &
+        rosrun unitree_camera_utils pub_image _run_config_file:=${SCRIPT_DIR}/config/stereo_camera_config.yaml camera${camNum2} &
     fi
     sleep 5
 fi
